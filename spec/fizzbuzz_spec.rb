0f1dacc64878cc146efe531f639a04a0e94bf89a
Jon Clarke <jonclarke1984@googlemail.com>
2022-03-02 16:27:20 +0000

    maliciously compliant

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,7 @@
-require './lib/fizzbuzz.rb'
+require 'fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to ez 'fizz'
+    expect(fizzbuzz(3)).to eq 'fizz'
   end
 end
