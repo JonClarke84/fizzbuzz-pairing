08716688fe54be524ed66036f36598186da44a14
Daniel Taplin <98479830+dantaplin992@users.noreply.github.com>
2022-03-02 16:47:24 +0000

    moretests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,11 @@
 def fizzbuzz(number)
-  if number == 3
+  if number % 3 == 0 && number % 5 == 0
+    'fizzbuzz'
+  elsif number == 3
     'fizz'
-  else
+  elsif number == 5
     'buzz'
+  else
+    number
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,4 +9,24 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
 
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+  it 'returns 4 when passed 4' do
+    expect(fizzbuzz(4)).to eq 4
+  end
+
+  it 'returns 11 when passed 11' do
+    expect(fizzbuzz(11)).to eq 11
+  end
+
+  it 'returns 73 when passed 73' do
+    expect(fizzbuzz(73)).to eq 73
+  end
+
+  it 'returns 12 when passed 12' do
+    expect(fizzbuzz(12)).to eq 12
+  end
+  
 end
